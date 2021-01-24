cda65f7446366006e96b255fe3b224a24701fbd2
EgorMezhin <49487690+EgorMezhin@users.noreply.github.com>
2021-01-24 22:11:24 +0300

    Add the ability to measure the distance between two points

---
+++ RulerApp-AR/AlertViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AlertViewController.swift
+//  RulerApp-AR
+//
+//  Created by Egor Lass on 24.01.2021.
+//
+
+import UIKit
+
+class AlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- RulerApp-AR/ViewController.swift
+++ RulerApp-AR/ViewController.swift
@@ -10,20 +10,22 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
+    var panelArray = [SCNNode]()
+    var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
+    
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var statusLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        statusLabel.text = "Wait"
+        statusLabel.text = "Detecting the surface"
         // Set the view's delegate
         sceneView.delegate = self
         
-        
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,7 +35,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let configuration = ARWorldTrackingConfiguration()
         
         configuration.planeDetection = .horizontal
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -45,8 +47,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    @IBAction func deleteButton(_ sender: UIBarButtonItem) {
+        
+        if !panelArray.isEmpty {
+            for panel in panelArray {
+                panel.removeFromParentNode()
+            }
+        }
+        
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+        }
         
         if let location = touches.first?.location(in: sceneView) {
             guard let query = sceneView.raycastQuery(from: location, allowing: .existingPlaneInfinite, alignment: .any) else {
@@ -59,7 +77,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 let alert = UIAlertController(title: "Surface not found yet", message: "Please wait for surface detection", preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
                 self.present(alert, animated: true, completion: nil)
-
+                
                 return
             }
             
@@ -67,7 +85,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             print("surface has been found")
         }
     }
-
+    
     
     func addDot(at hitResult : ARRaycastResult) {
         
@@ -83,42 +101,81 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.scene.rootNode.addChildNode(dotNode)
         
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            calculate()
+        }
+        
     }
     
-
+    func calculate() {
+        let start = dotNodes[0]
+        let end = dotNodes[1]
+        
+        let a = end.position.x - start.position.x
+        let b = end.position.y - start.position.y
+        let c = end.position.z - start.position.z
+        let distance = String(format: "%.2f", abs(sqrt(pow(a, 2) - pow(b, 2) - pow(c, 2))))
+        
+        updateText(text: "\(distance)", atPosition: end.position)
+    }
+    
+    func updateText(text: String, atPosition position: SCNVector3) {
+        
+        textNode.removeFromParentNode()
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+        
+        textNode.scale = SCNVector3(0.003, 0.003, 0.003)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+    }
+    
+    func removePanelNode(panelNode: SCNNode) {
+        panelNode.isHidden = true
+        panelNode.removeFromParentNode()
+    }
+    
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if anchor is ARPlaneAnchor {
             
+            
             DispatchQueue.main.async {
-                
-                self.statusLabel.text = "OK"
-                
+                self.statusLabel.isHidden = true
             }
-//            let planeAnchor = anchor as! ARPlaneAnchor
-//
-//            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
-//
-//            let planeNode = SCNNode()
-//
-//            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
-//
-//            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
-//
-//            let gridMaterial = SCNMaterial()
-//
-//            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
-//
-//            plane.materials = [gridMaterial]
-//
-//            planeNode.geometry = plane
-//
-//            node.addChildNode(planeNode)
+            
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+    
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+            panelArray.append(planeNode)
             
         } else {
             return
         }
     }
-
     
 }
 
